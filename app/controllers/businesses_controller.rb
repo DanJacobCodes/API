01819e1b3981f0dd45b247ecd68f4c6b170f759c
Dan Jacob <dansamueljacob@gmail.com>
2017-07-21 12:11:35 -0700

    Add FactoryGirl factory, and pass specs for get route

--- app/controllers/businesses_controller.rb
+++ app/controllers/businesses_controller.rb
@@ -1,12 +1,37 @@
 class BusinessesController < ApplicationController
 
   def index
-    @businesses = {"businesses": "TABLE"}
+    @businesses = Business.all
     json_response(@businesses)
   end
 
+  def show
+    @business = Business.find(params[:id])
+    json_response(@business)
+  end
+
+  def create
+    @business = Business.create(business_params)
+    json_response(@business)
+  end
+
+  def update
+    @business = Business.find(params[:id])
+    @business.update(business_params)
+  end
+
+  def destroy
+    @business = Business.find(params[:id])
+    @business.destroy
+  end
+
   private
+  
   def json_response(object)
     render json: object, status: :ok
+  end
+
+  def business_params
+    params.permit(:name)
   end
 end
