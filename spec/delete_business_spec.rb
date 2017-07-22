cad75afe6282527cfc938b7955656ba8f0a9ec52
Dan Jacob <dansamueljacob@gmail.com>
2017-07-21 19:41:33 -0700

    pass DELETE specs

--- spec/delete_business_spec.rb
+++ spec/delete_business_spec.rb
@@ -1,16 +1,13 @@
-
 require 'rails_helper'
-describe 'deletes one business', :type=>:request do
-  before {
-    @business = FactoryGirl.create(:business)
-    delete '/businesss/'+@business.id.to_s
-  }
 
-  it 'returns delete confirmation' do
-    expect(JSON.parse(response.body)['message']).to eq("Business removed")
+describe 'deletes one business', :type=>:request do
+  before do
+    business = FactoryGirl.create(:business)
+    delete '/businesses/' + business.id.to_s
   end
 
   it 'returns status code 200' do
-    expect(response).to have_http_status(:success)
+    expect(response).to have_http_status :ok
   end
+
 end
